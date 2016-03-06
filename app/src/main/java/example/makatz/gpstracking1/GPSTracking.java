e06dd2f69d8c991d51881a8b2afb83900401f214
Ma Katz <matn95@gmail.com>
2016-03-06 17:18:51 +0700

    First commit

---
+++ app/src/main/java/example/makatz/gpstracking1/GPSTracking.java
@@ -0,0 +1,13 @@
+package example.makatz.gpstracking1;
+
+import android.app.Application;
+
+/**
+ * Created by makatz on 3/6/2016.
+ */
+public class GPSTracking extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
